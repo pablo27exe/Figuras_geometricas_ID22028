e347581519bcbcbee8672e70132c9dee64352ef9
pablo27exe <163430211+pablo27exe@users.noreply.github.com>
2024-05-26 15:52:07 -0600

    Update Form1.cs

--- Figurasgeometricas/Form1.cs
+++ Figurasgeometricas/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +12,7 @@ namespace Figurasgeometricas
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +26,68 @@ namespace Figurasgeometricas
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void export_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void info_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void figura_CheckedChanged(object sender, EventArgs e)
+        {
+            if (figura.Checked)
+            {
+                Form2figuras figuras = new Form2figuras();
+                figuras.Show();
+                this.Hide();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                
+            
         }
     }
 }
